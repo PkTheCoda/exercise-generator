37e6191e8bd266d6b835e1a18640d25c8d52dcbd
PkTheCoda <107774675+PkTheCoda@users.noreply.github.com>
2023-05-23 19:14:37 -0400

    Added BodyContent.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./style.css";
 import Header from "./Header.js"
+import BodyContent from "./BodyContent.js"
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <BodyContent />
+    </>
   );
 }
---
+++ src/BodyContent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const BodyContent = () => {
+  return (
+    <div className="body-container">
+      <div className="exercise-items">1</div>
+      <div className="exercise-items">2</div>
+      <div className="exercise-items">3</div>
+    </div>
+  )
+}
+
+export default BodyContent
